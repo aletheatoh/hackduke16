6abaef668bbcb21ed9d27b8c1bfe844b0f9f9b93
Geng <snggeng@gmailcom>
2016-11-20 03:33:54 -0500

    test

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+before_action :configure_sign_up_params, only: [:create]
+before_action :configure_account_update_params, only: [:update]
+
+# before_filter :configure_permitted_parameters, :only => [:create]
+#
+# protected
+#
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :age, :gender, :nationality, :occupation, :dob) }
+#   end
 
   # GET /resource/sign_up
   # def new
@@ -39,14 +47,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :age, :gender, :nationality, :occupation, :dob])
+  end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:signup) << [:email, :password, :age, :gender, :nationality, :occupation, :dob]
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :age, :gender, :nationality, :occupation, :dob])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root 'users#index'
   resources :homes
-  devise_for :users
+  devise_for :users, :controllers => {:sessions => 'devise/sessions', :registrations => 'users/registrations'} do
+  # get "/login", :to => "devise/sessions#new", :as => :login
+  # get "/signup", :to => "devise/registrations#new", :as => :signup
+  # get "/logout", :to => "devise/sessions#destroy", :as => :logout
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
