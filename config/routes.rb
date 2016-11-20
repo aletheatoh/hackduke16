ebd59cbc8638aaad8d6200689a3ebcce44721488
Geng <snggeng@gmailcom>
2016-11-20 01:48:13 -0500

    rails g scaffold home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#sign_in'
+  resources :homes
+  root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
